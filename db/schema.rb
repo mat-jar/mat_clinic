94af76208f7d70c249882185a73e41d69ea8a9ff
Mateusz Jarczyński <mtu07@poczta.fm>
2023-07-19 00:03:29 +0200

    Add unique index for pesel in patients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_211111) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_220126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_211111) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["pesel"], name: "index_patients_on_pesel", unique: true
   end
 
 end
