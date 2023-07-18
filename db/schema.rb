ee6b1f2b4ac8fc25cee33c88bce0f305d0468cfe
Mateusz Jarczyński <mtu07@poczta.fm>
2023-07-19 01:25:18 +0200

    Add doctors to db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_220126) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_231254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
