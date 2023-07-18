8b34930ce9db894f900bb060d4681af65d3886a7
Mateusz Jarczyński <mtu07@poczta.fm>
2023-07-19 01:25:48 +0200

    Add table with pagination to doctors index

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,7 +3,7 @@ class DoctorsController < ApplicationController
 
   # GET /doctors or /doctors.json
   def index
-    @doctors = Doctor.all
+    @doctors = Doctor.page(params[:page])
   end
 
   # GET /doctors/1 or /doctors/1.json
