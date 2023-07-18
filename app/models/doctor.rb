425a8cd52c59a4c44024cbd0516f76b2a17a7c90
Mateusz Jarczyński <mtu07@poczta.fm>
2023-07-19 01:16:07 +0200

    Add Doctor model properties and validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,8 @@
 class Doctor < ApplicationRecord
+    validates :first_name, :last_name,  presence: true
+    validates :first_name, :last_name, format: { with: /\A\D*\z/, message: "cannot contain any digits" }
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
